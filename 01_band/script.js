9e8b9c3cb69b4d0ce06a15b909aee1ea81f86ed3
dragonbead95 <dragonbead95@naver.com>
2020-08-01 15:04:47 +0900

    complete band template

--- 01_band/script.js
+++ 01_band/script.js
@@ -15,3 +15,42 @@ function carousel() {
   slideIndex++;
   setTimeout(carousel, 2000);
 }
+
+const ticket__model = document.querySelector(".content__tour__ticket");
+const ticket__buy = document.querySelectorAll(".ticket__buy");
+const ticket__close = document.querySelectorAll(".js_close");
+
+function on_window_ticket() {
+  ticket__model.style.display = "block";
+}
+
+function close_window_ticket() {
+  ticket__model.style.display = "none";
+}
+
+ticket__buy.forEach((ticket) =>
+  ticket.addEventListener("click", on_window_ticket)
+);
+ticket__close.forEach((ticket) =>
+  ticket.addEventListener("click", close_window_ticket)
+);
+
+const send = document.querySelector(".js_send");
+
+function send_contact() {
+  const name = document.querySelector(".js_name");
+  const email = document.querySelector(".js_email");
+  const message = document.querySelector(".js_message");
+
+  if (name.value === "") {
+    name.focus();
+  }
+  if (email.value === "") {
+    email.focus();
+  }
+  if (message.value === "") {
+    message.focus();
+  }
+}
+
+send.addEventListener("click", send_contact);
